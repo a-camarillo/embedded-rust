7deedbf7e196e4a74b5dbff8742d51fd34e9c3c3
Anthony Camarillo <anthony.camarillo.96@gmail.com>
2024-04-24 09:33:27 -0700

    add flake for nix development

---
+++ flake.nix
@@ -0,0 +1,40 @@
+{
+  description = "Embedded Rust with the Microbit:v2";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+  };
+
+  outputs = inputs@{ self, nixpkgs, flake-parts, rust-overlay, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [ "x86_64-linux" ];
+      perSystem = {config, self', inputs', pkgs, system, ... }:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+      in
+      {
+        devShells = {
+          default = pkgs.mkShell {
+            nativeBuildInputs = with pkgs; [
+              (rust-bin.stable.latest.default.override {
+                extensions = [ "rust-src" ];
+                targets = [ "thumbv7em-none-eabihf" ];
+              })
+              rustc
+              cargo
+              probe-rs
+              gdb
+              minicom
+              cargo-binutils
+            ];
+            RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
+          };
+        };
+      };
+  };
+}
